4fe3a0dbaf286bf62fa6b5266169262980fb90be
Sanne Bregman <sanne@bregman.email>
2021-09-05 19:26:17 +0200

    Initial commit or something

---
+++ src/typescript/index.ts
@@ -0,0 +1,11 @@
+import "../sass/index.scss";
+
+function component() {
+    const element = document.createElement('div');
+
+    element.innerHTML = ' '.concat('Hello', 'webpack');
+
+    return element;
+}
+
+document.body.appendChild(component());
